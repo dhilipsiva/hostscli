32b1f36a1fa5ce39d85cec540d157c6c6fdb331b
dhilipsiva <dhilipsiva@gmail.com>
2016-12-28 20:33:21 +0530

    Add a long description

--- setup.py
+++ setup.py
@@ -18,11 +18,61 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 f = path.join(here, 'README.md')
 
+# pandoc -t rst README.md | pbcopy
+long_description = """
+hostscli
+========
+
+A CLI tool to block / unblock websites using /etc/hosts. Super simple
+and easily extendable.
+
+Installation
+------------
+
+::
+
+    pip install -U hostscli
+
+Usage
+-----
+
+\*\*\* Please note that we require ``sudo`` permissions to change
+``/etc/hosts`` file \*\*\*
+
+To block ``facebook``:
+
+::
+
+    sudo hostscli block facebook
+
+To unblock ``facebook``:
+
+::
+
+    sudo hostscli block facebook
+
+To list supported websites:
+
+::
+
+    hostscli websites
+
+More websites?
+--------------
+
+If you need to add more websites, just create a ``<website>.py`` file in
+``hostscli/websites/`` directory with list of domains declared as
+variable ``DOMAINS``. Please look at ``hostscli/websites/facebook.py``
+file for reference
+
+"""
+
 setup(
     name='hostscli',
     version='0.0.3',
     description=(
         "hostscli is a CLI tool to block / unblock websites using /etc/hosts"),
+    long_description=long_description,
     url='https://github.com/dhilipsiva/hostscli',
     author='dhilipsiva',
     author_email='dhilipsiva@gmail.com',
