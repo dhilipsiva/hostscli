833e717c6b41a7254fbc5b83103c577d346add28
dhilipsiva <dhilipsiva@gmail.com>
2016-12-28 18:41:59 +0530

    hostscli

---
+++ setup.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: setup.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2016-12-28
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+f = path.join(here, 'README.md')
+
+setup(
+    name='hostscli',
+    version='0.0.1',
+    description=(
+        "hostscli is a CLI tool to block / unblock websites using /etc/hosts"),
+    url='https://github.com/dhilipsiva/hostscli',
+    author='dhilipsiva',
+    author_email='dhilipsiva@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Networking :: Firewalls',
+        'Topic :: Utilities',
+    ],
+
+    keywords='hostscli hosts /etc/hosts block unblock websites',
+    packages=find_packages(),
+    py_modules=['hostscli'],
+    entry_points='',
+    install_requires=[],
+)
