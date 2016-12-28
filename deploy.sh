195b76d6c211b6cad26807073167ad2d4d0651fe
dhilipsiva <dhilipsiva@gmail.com>
2016-12-28 20:17:38 +0530

    Add a bumpversion file

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 #
 # deploy.sh
-# Copyright (C) 2015 dhilipsiva <dhilipsiva@gmail.com>
+# Copyright (C) 2016 dhilipsiva <dhilipsiva@gmail.com>
 #
 # Distributed under terms of the MIT license.
 #
 
+echo "Deploying!"
 
 rm -rf dist/
+bumpversion patch
 python setup.py sdist
-python setup.py bdist_wheel
 twine upload dist/*
+git push
+git push --tags
