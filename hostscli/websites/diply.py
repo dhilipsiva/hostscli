a2f5681fa8597c96043a20a6fdcb6221b5b7f92d
dhilipsiva <dhilipsiva@gmail.com>
2016-12-31 12:37:33 +0530

    Added diply website

---
+++ hostscli/websites/diply.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: diply.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2016-12-31
+"""
+
+DOMAINS = [
+    'diply.com',
+]
