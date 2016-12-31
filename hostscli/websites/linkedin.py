aaea628e85bb4069a24e9f4b3f98e5e96c50ae62
dhilipsiva <dhilipsiva@gmail.com>
2016-12-31 17:25:40 +0530

    Add more sites

---
+++ hostscli/websites/linkedin.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: linkedin.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2016-12-31
+"""
+
+DOMAINS = [
+    'linkedin.com',
+    'www.linkedin.com',
+]
---
+++ hostscli/websites/twitter.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: twitter.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2016-12-31
+"""
+
+DOMAINS = [
+    'twitter.com',
+    'www.twitter.com',
+]
