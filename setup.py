0c66dca5ca67dc86903d5c64936b3550ade6062f
dhilipsiva <dhilipsiva@gmail.com>
2017-01-15 13:46:58 +0530

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ file for reference
 
 setup(
     name='hostscli',
-    version='1.0.0',
+    version='1.0.1',
     description=(
         "hostscli is a CLI tool to block / unblock websites using /etc/hosts"),
     long_description=long_description,
