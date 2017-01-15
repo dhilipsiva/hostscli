0a886370055689433ea0f6fcd19662430305fbff
dhilipsiva <dhilipsiva@gmail.com>
2017-01-15 19:18:27 +0530

    Update FB, add Instagram & WhatsApp

---
+++ hostscli/websites/whatsapp.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: whatsapp.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2017-01-15
+"""
+
+DOMAINS = [
+    "w1.web.whatsapp.com",
+    "w2.web.whatsapp.com",
+    "w3.web.whatsapp.com",
+    "w4.web.whatsapp.com",
+    "w5.web.whatsapp.com",
+    "w6.web.whatsapp.com",
+    "w7.web.whatsapp.com",
+    "w8.web.whatsapp.com",
+    "w9.web.whatsapp.com",
+    "web.whatsapp.com",
+    "whatsapp-cdn-shv-01-atl1.fbcdn.net",
+    "whatsapp-cdn-shv-01-dfw1.fbcdn.net",
+    "whatsapp-cdn-shv-01-iad3.fbcdn.net",
+    "whatsapp-cdn-shv-01-lax1.fbcdn.net",
+    "whatsapp-cdn-shv-01-lax3.fbcdn.net",
+    "whatsapp-cdn-shv-01-lga1.fbcdn.net",
+    "whatsapp-cdn-shv-01-mia1.fbcdn.net",
+    "whatsapp-cdn-shv-01-ord1.fbcdn.net",
+    "whatsapp-cdn-shv-01-sea1.fbcdn.net",
+    "whatsapp-cdn-shv-01-sjc2.fbcdn.net",
+    "whatsapp.com",
+    "www.whatsapp.com",
+]
