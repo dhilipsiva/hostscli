32e328f322cdc6fe62e219ff995482242e914ecc
dhilipsiva <dhilipsiva@gmail.com>
2017-01-15 19:47:08 +0530

    Add pintrest

---
+++ hostscli/websites/pintrest.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: pintrest.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2017-01-15
+"""
+
+DOMAINS = [
+    "about.pinterest.com",
+    "ads.pinterest.com",
+    "api.pinterest.com",
+    "blog.pinterest.com",
+    "br.pinterest.com",
+    "business.pinterest.com",
+    "developers.pinterest.com",
+    "developers.pinterest.com",
+    "engineering.pinterest.com",
+    "help.pinterest.com",
+    "id.pinterest.com",
+    "pinterest.com",
+    "uk.pinterest.com",
+    "www.pinterest.com",
+]
